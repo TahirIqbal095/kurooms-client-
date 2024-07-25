12dfd66eb6aa41b658a9646d839ff1c20fa0d083
Tahir Iqbal <shahtahir786@gmail.com>
2024-07-25 16:55:03 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Hello world</div>
+    </>
+  );
+}
+
+export default App;
